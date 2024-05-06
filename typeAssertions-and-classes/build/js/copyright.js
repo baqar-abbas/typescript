44602a764d365584624d925df406f61f26782fa8
baqar-abbas <baqar007@hotmail.com>
2024-05-06 21:13:22 +0500

    Worked on TypeScript type Assertions and classes

---
+++ typeAssertions-and-classes/build/js/copyright.js
@@ -0,0 +1,18 @@
+"use strict";
+//Original JS Code
+// const year = document.getElementById('year');
+// const currentYear = new Date().getFullYear();
+// year.setAttribute('datetime', currentYear.toString());
+// year.textContent = currentYear.toString();
+// // 1st variation
+// let year: HTMLElement | null = document.getElementById('year');
+// let currentYear: string = new Date().getFullYear().toString();
+// if (year) {
+//     year.setAttribute('datetime', currentYear);
+//     year.textContent = currentYear;
+// }
+// 2nd variation
+const year = document.getElementById('year');
+const currentYear = new Date().getFullYear().toString();
+year.setAttribute('datetime', currentYear);
+year.textContent = currentYear;
